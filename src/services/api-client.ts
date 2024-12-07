ffa1550144727baa324e082fb9719502ed30fb59
Shahebaz Khan Pathan <146757560+ShahebazKhanPathan@users.noreply.github.com>
2024-12-07 17:21:30 +0530

    Update api-client.ts

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "https://snapstyle-backend-3.vercel.app/"
+    baseURL: "https://snapstyle-backend-3-i2bekddno-shahebaz-khan-pathans-projects.vercel.app/"
 })
